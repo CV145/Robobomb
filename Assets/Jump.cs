e7d851beeb5b69f5f9a98fc4f6d50a6d6ce1574f
CV145 <carlosvaleriano45@yahoo.com>
2018-10-20 11:37:10 -0500

    Bun o now faces Robo

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -2,21 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour {
+public class Jump : MonoBehaviour
+{
 
     public LayerMask whatIsGround;
     Rigidbody2D rigidbody;
+    Transform player;
+    bool facingRight;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rigidbody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        facingRight = false;
+    }
+
+    private void Awake()
+    {
+        player = GameObject.Find("RoboPlayer").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 4), 0.4f, whatIsGround))
-            {
+        {
             rigidbody.velocity = Vector2.up * 25;
         }
+
+
+        if (player.position.x > transform.position.x && !facingRight)
+        {
+            Flip();
+            facingRight = true;
+        }
+        if (player.position.x < transform.position.x && facingRight)
+        {
+            Flip();
+            facingRight = false;
         }
     }
+
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1; //go from 1 to -1 to 1 again
+        transform.localScale = theScale;
+    }
+}
