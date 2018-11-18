5955c7d8034025b0b29784b370d71b41403947a0
CV145 <carlosvaleriano45@yahoo.com>
2018-11-17 19:48:26 -0600

    Test banner ads now working!

---
+++ Assets/Admanager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class Admanager : MonoBehaviour {
+
+    public static Admanager instance;
+
+    //dummy ads still loading even without IDs
+    private string appID = "ca-app-pub-1906605411705517~2269664916";
+
+    private BannerView bannerView;
+    private string bannerID = "ca-app-pub-3940256099942544/6300978111";
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        RequestBanner();
+    }
+
+    public void RequestBanner()
+    {
+        bannerView = new BannerView(bannerID, AdSize.Banner, AdPosition.Bottom);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        bannerView.LoadAd(request);
+
+        bannerView.Show();
+    }
+
+
+    public void HideBanner()
+    {
+        bannerView.Hide();
+    }
+}
